28abb12c0fa31376159f008ddbf9ebf4a5a28106
denvysotskyi <denvysotskyi@gmail.com>
2021-08-14 12:35:28 +0300

    Modify PostController

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,10 +10,28 @@ class PostController {
     }
   }
 
+  async getOne(req, res) {
+    try {
+      const post = await PostService.getOne(req.params.id)
+      return res.json(post)
+    } catch (e) {
+      res.status(500).json(e.message)
+    }
+  }
+
   async create(req, res) {
     try {
       const post = await PostService.create(req.body)
       res.status(200).json(post)
+    } catch (e) {
+      res.status(500).json(e.message)
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const updatedPost = await PostService.update(req.body)
+      res.status(200).json(updatedPost)
     } catch (e) {
       res.status(500).json(e.message)
     }
