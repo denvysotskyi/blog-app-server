105681f1f2b873c740545391119b3876440a751c
denvysotskyi <denvysotskyi@gmail.com>
2021-08-21 12:04:42 +0300

    Modify model

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require('mongoose')
 
-const Post = new Schema({
+const postSchema = new Schema({
   title: {type: String, required: true},
   text: {type: String, required: true},
   imageUrl: {type: String, required: true}
 }, {timestamps: true})
 
-module.exports = model('Post', Post)
+module.exports = model('Post', postSchema)
